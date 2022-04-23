ed0b63e12de8c8b68017cef3c871171b93726748
Jacob Simonek <78231728+Simoja0428@users.noreply.github.com>
2022-04-23 16:16:50 -0500

    Formatting
    Added formatting for login screen, specifically label for login and password container.

--- spotlight_app/lib/loginscreen.dart
+++ spotlight_app/lib/loginscreen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-class loginscreen extends StatelessWidget {
+class loginscreen extends StatelessWidget 
+{
+  final String loginLabel;
+  final String passwordLabel;
+
+  loginscreen(this.loginLabel, this.passwordLabel);
 
   @override
   Widget build(BuildContext context)
   {
-    
+    return Container(
+      color: Theme.of(context).primaryColorLight,
+      child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Column: (
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      loginLabel,
+                      ),
+                    Text(
+                      passwordLabel,
+                     ),
+                  ])),
+          ],
+      )
+    );
   }
 }
