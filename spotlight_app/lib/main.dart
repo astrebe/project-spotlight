63b499d41d9886dfcd844dacde6dd28da646a209
Jacob Simonek <78231728+Simoja0428@users.noreply.github.com>
2022-04-23 15:50:22 -0500

    login screen class
    added login screen class

--- spotlight_app/lib/main.dart
+++ spotlight_app/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'loginscreen.dart';
 
 void main() {
   runApp(const MyApp());
-  ///happy Halloween!!!
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login Screen',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,9 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'SpotLight Login Screen'),
     );
   }
 }
