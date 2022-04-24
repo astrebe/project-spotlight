d53b5b554274a789d71dc4b3c051ce0cbe0a03d3
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-04-24 16:56:02 -0500

    Create Filter Class and View

---
+++ spotlight_app/lib/SpotlightFilterView.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class Spotlightfilterview extends StatefulWidget {
+  const Spotlightfilterview({Key? key}) : super(key: key);
+
+  @override
+  _SpotlightfilterviewState createState() => _SpotlightfilterviewState();
+}
+
+class _SpotlightfilterviewState extends State<Spotlightfilterview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
