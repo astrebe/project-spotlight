23be2a092921ac9326c3ee6a3f3d81a24906c708
astrebe <astrebe@carthage.edu>
2022-05-16 12:47:20 -0500

    realized that github only hosts static pages, removed handlebars & updated index.html

--- docs/index.js
+++ docs/index.js
@@ -1,23 +1,11 @@
 'use strict';
-
-const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-
-app.engine('handlebars', handlebars.engine); 
-app.set('view engine', 'handlebars'); 
-app.set('views', './views');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/', async (req, res) => {
-    res.render('home');
-});
+app.use(express.static('public')); 
 
 app.use((req, res) => {
     res.status(404).send(`Error 404: ${req.url} cannot be found :(`);
